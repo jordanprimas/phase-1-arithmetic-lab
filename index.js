effcb41c0d1d6a96a738fc74eb5b8319a9560e58
Jordan Primas <jorprimas20@gmail.com>
2023-02-09 09:18:50 -0800

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 62
+let num2 = 1
+const multiply = num1 * num2
+console.log(multiply)
 
-// Write your code here
+const random = Math.floor(Math.random() * 20)+1 
+
+let num3 = 16
+let num4 = 4
+const mod = num3 / num4
+console.log(mod)
+
+const max = Math.max(5,10,15,20)
